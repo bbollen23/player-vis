6d71dafda7ed520f1a1ecd73826581e063f4b5b6
Brian Bollen <bbollen@gravyanalytics.com>
2023-12-24 13:21:16 -0700

    First commit. Basic struture of Flask application. Added players endpoints

---
+++ api/app.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+# Get current working directory for the main file.
+current_dir = os.path.dirname(os.path.realpath(__file__))
+# Get the parent directory
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+# Append the parent directory to the system path so allow imports from 'api'.
+sys.path.append(parent_dir)
+
+
+from flask import Flask, json
+from api.players.players import players_bp
+
+
+# Set the static folder and the static url path for the React application
+app = Flask(__name__,static_folder='../build',static_url_path='/')
+app.register_blueprint(players_bp)
+
+# Route which returns the index.html file of the react application
+@app.route("/",methods=['GET'])
+def index():
+  return app.send_static_file('index.html')
+
+
---
+++ api/players/players.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request,jsonify,render_template
+import os
+import json
+players_bp = Blueprint('players',__name__,url_prefix='/players')
+
+'''Route returning all players'''
+@players_bp.route('/',methods=['GET'])
+def get_all_players():
+  # Load JSON file (no need for additional directory information -- we append the root directory on creation of the Flask App)
+  data = json.load(open('soccer_small.json'))
+  
+  return jsonify(data),200
+
+'''Route returning specific player based on URL path'''
+@players_bp.route('/<player_name>',methods=['GET'])
+def get_player(player_name):
+  # Load JSON file (no need for additional directory information -- we append the root directory on creation of the Flask App)
+  data = json.load(open('soccer_small.json'))
+  
+  # Search through player database
+  for player in data:
+    #A match is constituted by the inputted name with no spaces matching the player's name in the JSON file with no whitespaces. 
+    # This is not case sensitive.
+    
+    if player['Name'].lower().replace(' ','') == player_name.lower():
+      return jsonify(player),200
+  
+  # If no player was found, return an error.
+  return jsonify({'error':'NO_PLAYER_FOUND','error_message':'No player was found with this name.'}),200  
